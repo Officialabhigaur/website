febb8aef5b2a5e377c1ccb7cc478ebaeabb44204
Abhishek Gaur <agaur0136@gmail.com>
2025-10-02 23:07:18 +0530

    new types folder created

---
+++ src/types/acternity-ui.d.ts
@@ -0,0 +1,8 @@
+// types/acternity-ui.d.ts
+
+declare module 'acternity-ui' {
+  // Treat the entire library as 'any'
+  const ActernityUI: any;
+  export = ActernityUI;
+  export default ActernityUI;
+}
